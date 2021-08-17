e445730b07aca168456a2431205525fed156d186
Jessica Felts <thejessicafelts@gmail.com>
2021-08-17 11:43:00 -0400

    Updates to JS

--- proof.js
+++ proof.js
@@ -1,31 +1,74 @@
-// Number Limit
+// Number Limit (9007199254740991)
 let maxNumber = Number.MAX_SAFE_INTEGER;
 
-// Enter a Number
-const userNumber = prompt("Enter a number: ");
+/*   
+   Validating the user entry.
+   - Make sure entry is a number. Else, return error.
+   - Make sure entry is positive. Else, return error.
+   - Make sure entry is a whole number. Else, return error.
+   - Make sure entry is below maxNumber. Else, return error.
+   - Submit only once conditions are met.
+*/
 
-// Collatz Conjecture
-if(userNumber <= maxNumber) {
+// Prevent Form from Resetting
+document.querySelector("#userSubmit").addEventListener("click", function(event){
+   event.preventDefault();
+}, false);
 
-   // Check Even or Odd
-   checkNumber = (userNumber % 2 == 0) ? "even" : "odd";
-   console.log(`${userNumber} is ${checkNumber}.`);
-
-   // If Even
-   if(checkNumber == "even"){
-      let newNumber = userNumber / 2;
-      console.log(`The new number is ${newNumber}`);
-   }
-
-   // If Odd
-   if(checkNumber == "odd"){
-      let newNumber = ((userNumber * 3) + 1);
-      console.log(`The new number is ${newNumber}`);
-   }
-
-} else {
-   alert("Number is too large.")
+// Submitting and Saving User Input
+function submit_form(){
+   let userInput = document.getElementById("userInput").value;
+   console.log(`User Entered: ${userInput}.`);
 }
 
 
 
+
+// // Collatz Conjecture
+// if(userNumber <= maxNumber) {
+
+//    while(userNumber < 1) {
+
+//       // Check Even or Odd
+//       checkNumber = (userNumber % 2 == 0) ? "even" : "odd";
+//       console.log(`${userNumber} is ${checkNumber}.`);
+
+//       // If Even
+//       if(checkNumber == "even"){
+//          let userNumber = userNumber / 2;
+//          console.log(`The new number is ${userNumber}`);
+//       }
+
+//       // If Odd
+//       if(checkNumber == "odd"){
+//          let userNumber = ((userNumber * 3) + 1);
+//          console.log(`The new number is ${userNumber}`);
+//       }
+
+//    }
+//    console.log(`${userNumber} has reached 1`).
+
+// } else {
+//    alert("Number is too large.")
+// }
+
+// while(userNumber < 1) {
+
+//       // Check Even or Odd
+//       checkNumber = (userNumber % 2 == 0) ? "even" : "odd";
+//       console.log(`${userNumber} is ${checkNumber}.`);
+
+//       // If Even
+//       if(checkNumber == "even"){
+//          let userNumber = userNumber / 2;
+//          console.log(`The new number is ${userNumber}`);
+//       }
+
+//       // If Odd
+//       if(checkNumber == "odd"){
+//          let userNumber = ((userNumber * 3) + 1);
+//          console.log(`The new number is ${userNumber}`);
+//       }
+
+//    }
+//    console.log(`${userNumber} has reached 1`).

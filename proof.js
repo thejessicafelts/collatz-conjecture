69829dcecf3cbba841d519f4fa0c3c126ab85c3b
Jessica Felts <thejessicafelts@gmail.com>
2021-08-19 11:32:14 -0400

    Updates to UI and Information

--- proof.js
+++ proof.js
@@ -1,5 +1,5 @@
 // Number Limit (9007199254740991)
-let maxNumber = Number.MAX_SAFE_INTEGER;
+var maxNumber = Number.MAX_SAFE_INTEGER;
 
 // Prevent Form from Resetting
 document.querySelector("#userSubmit").addEventListener("click", function(event){
@@ -9,99 +9,127 @@ document.querySelector("#userSubmit").addEventListener("click", function(event){
 // Submitting and Saving User Input
 function submit_form(){
 
-   var stepCounter = 1;
+   // Clear Previous Entries
+   document.getElementById("outputSummary").innerHTML = '';
+   document.getElementById("outputTableBody").innerHTML = '';
+   document.getElementById("outputTableSummary").innerHTML = '';
+   document.getElementById("outputGraph").innerHTML = '';
+
+   // Start Step Counter
+   var stepCounter = 0;
 
    // Set value of "userInput" when form is submitted
    var originalInput = document.getElementById("userInput").value;
-   var userInput = document.getElementById("userInput").value;  
+   var userInput = originalInput;  
 
-   if (userInput <= maxNumber){
+   // Show Output Section
+   document.getElementById("output").style.display = "block";
 
-      document.getElementById("outputTable").innerHTML += `The user entered: ${userInput}.<br /><br /><strong>Step ${stepCounter}</strong><br />`;
+   if (userInput <= maxNumber) {
+
+      document.getElementById("outputSummary").innerHTML += `You entered: <strong>${userInput}</strong><br/>`;
 
       // While the sequence is greater than 1...
       while ((userInput > 1) && (userInput <= maxNumber)) {
 
-         // Check if userInput is Even or Odd...
-         var checkNumber = (userInput % 2 == 0) ? "even" : "odd";
+         stepCounter++;
 
-         // If userInput is Even...
-         if (checkNumber == "even") {
+         // Check if userInput is "even" or "odd"...
+         var stateEvenOdd = (userInput % 2 == 0) ? "even" : "odd";
 
-            document.getElementById("outputTable").innerHTML += `${userInput} is ${checkNumber}.<br /> n = (n / 2) will be performed. <br />`;
+         // If stateEvenOdd is "even"...
+         if (stateEvenOdd == "even") {
 
-            var userInput = (userInput / 2);
-            stepCounter++; 
+            equation = `n = (n / 2)`;
+            newNum = (userInput / 2);
 
-            document.getElementById("outputTable").innerHTML += `The new number is: ${userInput}.<br /><br /><strong>Step ${stepCounter}</strong><br />`;
+            document.getElementById("outputTableBody").innerHTML += 
+            `
+            <tr>
+               <td class="step">Step ${stepCounter}</th>
+               <td class="num-start">${userInput}</th>
+               <td class="num-status">${stateEvenOdd}</th>
+               <td class="equation">${equation}</th>
+               <td class="num-end">${newNum}</th>
+            </tr>
+            `;
 
-         } 
-         
-         // If userInput is Odd... 
-         else if (checkNumber == "odd") {
+            userInput = newNum;
 
-            document.getElementById("outputTable").innerHTML += `${userInput} is ${checkNumber}.<br /> n = ((n * 3) + 1) will be performed. <br />`;
+         }
 
-            var userInput = ((userInput * 3) + 1);
-            stepCounter++;
+         // If stateEvenOdd is "odd"...
+         else if (stateEvenOdd == "odd") {
 
-            document.getElementById("outputTable").innerHTML += `The new number is: ${userInput}.<br /><br /><strong>Step ${stepCounter}</strong><br />`;
+            equation = `n = ((n * 3) + 1)`;
+            newNum = ((userInput * 3) + 1);
 
-         } 
-         
-         // If userInput is somehow neither Even nor Odd...
+            document.getElementById("outputTableBody").innerHTML += 
+            `
+            <tr>
+               <td class="step">Step ${stepCounter}</th>
+               <td class="num-start">${userInput}</th>
+               <td class="num-status">${stateEvenOdd}</th>
+               <td class="equation">${equation}</th>
+               <td class="num-end">${newNum}</th>
+            </tr>
+            `;
+
+            userInput = newNum;
+
+         }
+
+         // If stateEvenOdd is neither "even" nor "odd"...
          else {
 
-            // Add this snippet to the UI and remove from console
-            console.log(`ERROR: Cannot determine if ${userInput} is Even or Odd...`)
-         
+            document.getElementById("outputTableBody").innerHTML += 
+            `
+            <tr class="error">
+               <td class="step">Step ${stepCounter}</th>
+               <td class="num-start"><em>error</em></th>
+               <td class="num-status"><em>error</em></th>
+               <td class="equation"><em>error</em></th>
+               <td class="num-end"><em>error</em></th>
+            </tr>
+            `;
+
          }
-         
-      }
-
-      if (userInput > maxNumber) {
-
-         // Add this snippet to the UI and remove from console
-         console.log(`${userInput} is too large; please enter a smaller number.`);
-         console.log("Sequence terminated.");
-
-      }
-
-      if (userInput == 1) {
-
-         // Unhide the Output Section
-         document.getElementById("output").style.display = "block";
-
-         // Output Summary, based on stepCounter value
-         if (stepCounter > 1) {
-
-            var outputSummary = `It took <strong>${stepCounter} steps</strong> for ${originalInput} to reach 1.`;
-         
-         }
-         else if (stepCounter == 1) {
-         
-            var outputSummary = `It took <strong>${stepCounter} step</strong> for ${originalInput} to reach 1.`;
-         
-         }
-         else {
-         
-            var outputSummary = `ERROR: Something went wrong!`;
-         
-         }
-
-         // Print the outputSummary onto the UI
-         document.getElementById("outputSummary").innerHTML = outputSummary;
-
-         // Print the last line of outputTable
-         document.getElementById("outputTable").innerHTML += `The sequence has reached 1.<br />Sequence terminated.`;
 
       }
 
-   } else {
+      // Once the sequence reaches 1... 
+      document.getElementById("outputTableSummary").innerHTML +=
+      `
+      <p>The sequence has reached 1.</p>
+      <p>Sequence terminated.</p>
+      <p>It took ${stepCounter} steps for ${originalInput} to reach 1.</p>
+      `;
 
-      // Add this snippet to the UI and remove from console
-      console.log(`${userInput} is too large; please enter a smaller number.`);
-
+   } 
+   
+   // If userInput is larger than maxNumber...
+   else if (userInput > maxNumber) {
+      document.getElementById("outputSummary").innerHTML += 
+      `
+      <div class="alert error">
+         <strong>ERROR:</strong> ${userInput} is too large;<br />
+         Please enter a smaller number.
+      </div>
+      `;
+   }
+   
+   // If userInput has no relation to maxNumber...
+   else {
+      document.getElementById("outputSummary").innerHTML += 
+      `
+      <div class="alert error">
+         <strong>ERROR:</strong> Something went wrong!
+      </div>
+      `;
    }
 
+   // Reset Form
+   document.getElementById('userInputForm').reset();
+
 }
+

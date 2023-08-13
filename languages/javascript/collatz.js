1b6360c49bb9e33b57d470d4047b22b736d61000
Jessica Felts <thejessicafelts@gmail.com>
2023-08-13 10:13:48 -0400

    Adding spacing to JS solution

--- languages/javascript/collatz.js
+++ languages/javascript/collatz.js
@@ -11,27 +11,27 @@ if(isNaN(userInput) || userInput <= 0){
 function collatzConjecture(n){
     let steps = 0;
     if(n !== 1){
-        console.log(`User Input: ${userInput}.\n${n} is not equal to 1.\nBegin calculation...`);
+        console.log(`\nUser Input: ${userInput}.\n\n${n} is not equal to 1.\nBegin calculation...`);
     }
     while (n !== 1){
         if(n % 2 === 0){
             const nextNumber = n / 2;
             if(nextNumber == 1){
-                console.log(`Step ${(steps + 1)}\n--------\n${n} is even.\n(${n} / 2) = ${nextNumber}.\n${nextNumber} is equal to 1.\nCalculation complete.`);
+                console.log(`\nStep ${(steps + 1)}\n--------\n${n} is even.\n(${n} / 2) = ${nextNumber}.\n${nextNumber} is equal to 1.\nCalculation complete.`);
             } else {
-                console.log(`Step ${(steps + 1)}\n--------\n${n} is even.\n(${n} / 2) = ${nextNumber}.\n${nextNumber} is not equal to 1.\nContinue calculation...`);
+                console.log(`\nStep ${(steps + 1)}\n--------\n${n} is even.\n(${n} / 2) = ${nextNumber}.\n${nextNumber} is not equal to 1.\nContinue calculation...`);
             }
             n = nextNumber;
         } else {
             const nextNumber = ((n * 3) + 1);
             if(nextNumber == 1){
-                console.log(`Step ${(steps + 1)}\n--------\n${n} is odd.\n((${n} * 3) + 1) = ${nextNumber}.\n${nextNumber} is equal to 1.\nCalculation complete.`);
+                console.log(`\nStep ${(steps + 1)}\n--------\n${n} is odd.\n((${n} * 3) + 1) = ${nextNumber}.\n${nextNumber} is equal to 1.\nCalculation complete.`);
             } else {
-                console.log(`Step ${(steps + 1)}\n--------\n${n} is odd.\n((${n} * 3) + 1) = ${nextNumber}.\n${nextNumber} is not equal to 1.\nContinue calculation...`);
+                console.log(`\nStep ${(steps + 1)}\n--------\n${n} is odd.\n((${n} * 3) + 1) = ${nextNumber}.\n${nextNumber} is not equal to 1.\nContinue calculation...`);
             }
             n = nextNumber;
         }
         steps++;
     }
-    console.log(`${userInput} reached 1 in ${steps} steps.`);
+    console.log(`\n${userInput} reached 1 in ${steps} steps.`);
 }
